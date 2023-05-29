ce31db87af1d46ed56455050be34730ee4a38d3f
Iori Ide <k23gjk03@st.kyusan-u.ac.jp>
2023-05-29 17:00:28 +0900

    file open doの形式に変更

--- fileio2.rb
+++ fileio2.rb
@@ -3,6 +3,6 @@ print "name?"
 name = gets
 #name.txt というファイルに変数 name の値を保存
 
-file = File.open "name.txt", "w"
-file.puts name
-file.close
+File.open "name.txt", "w" do |file|
+    file.puts name
+end

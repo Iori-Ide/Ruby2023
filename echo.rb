c332c254c6e3267dbc9bcc5e2c2bb19fc24c296c
Iori Ide <k23gjk03@st.kyusan-u.ac.jp>
2023-06-05 16:01:08 +0900

    whileループでecho

--- echo.rb
+++ echo.rb
@@ -1,3 +1,14 @@
 # コマンドライン引数の表示
-msg = ARGV[0]
-puts msg
+#msg = ARGV
+#len = msg.length
+#len.times do |i|
+#    print msg[i] + " "
+#end
+#puts
+
+i=0
+while i<ARGV.length
+    print ARGV[i] + " "
+    i+=1
+end
+puts

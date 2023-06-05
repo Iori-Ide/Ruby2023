68a6d90397123c00df6e31fbb72aebd252c155c6
Iori Ide <k23gjk03@st.kyusan-u.ac.jp>
2023-06-05 17:16:12 +0900

    -nのオプションを追加

--- cat.rb
+++ cat.rb
@@ -1,6 +1,18 @@
 #! /usr/bin/env ruby
-File.open(ARGV[0], "r") do |file|
-    while line = file.gets
-        puts line
+
+nopt=false
+if "-n" == ARGV[0]
+    nopt=true
+    file=ARGV[1]
+else
+    nopt=false
+    file=ARGV[0]
+end
+linenum=1
+File.open(file, "r") do |f|
+    while line = f.gets
+        printf "%02d ",linenum if nopt
+            linenum+=1
+            puts line
     end
 end

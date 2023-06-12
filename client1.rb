257d0a86169401e0510838907929249372652343
Iori Ide <k23gjk03@st.kyusan-u.ac.jp>
2023-06-12 16:44:50 +0900

    HTTP1.0 ヘッド情報を受け取る

--- client1.rb
+++ client1.rb
@@ -2,8 +2,13 @@ require "socket"
 
 port = 80
 s=TCPSocket.open("www.is.kyusan-u.ac.jp",port)
-s.puts("GET /~toshi/")
+s.puts("GET /~toshi/ HTTP/1.0")
+s.puts
 while line=s.gets
+    line.chomp!
+    break if line == ""
+    #line.chomp!
     puts line
 end
+
 s.close
